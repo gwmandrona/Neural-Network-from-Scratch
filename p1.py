ce969bc383f0a0af1fcfa83bae253e109a7e51f0
Garrett Mandrona <gwmandrona@gmail.com>
2024-01-30 11:35:55 -0600

    Initial example of calculating some inputs and weight for a single neuron

---
+++ p1.py
@@ -0,0 +1,10 @@
+#simple example of a single neuron in a neural network
+inputs = [1, 2, 3]
+weights = [0.2, 0.8, -0.5]
+bias = 2
+
+
+
+output = inputs[0]*weights[0]  + inputs[1]*weights[1] + inputs[2]*weights[2] + bias
+print(output)
+
